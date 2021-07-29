795720d7882b03dd13b28a76e4237b5a8e2cd83b
alejogb1 <devsalejo@gmail.com>
2021-07-29 11:38:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Box,
+  Center,
+  Button, ButtonGroup, Field
+} from "@chakra-ui/react"
+import axios from "axios"
+import {useState} from "react"
+function App() {
+
+  const [data, setData] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("DATA: ", data)
+    axios({
+      method : "POST",
+      url: "http://localhost:3001/",
+      headers: {
+        "Content-type" : "application/json",
+      },
+      data : JSON.stringify({ email : data})
+    })
+  }
+
+  const handleEmail = (event) => {
+    setData(event.target.value)
+  }
+  
+  return (
+    <Center>
+      <Box style={{width: "80%"}} >
+        <Box >
+            <Center>
+                <form onSubmit={handleSubmit}>
+                  <FormControl>
+                    <FormLabel>Email</FormLabel>
+                    <Input type="email" placeholder="test@test.com" value={data} onChange={handleEmail}/>
+                  </FormControl>
+                  <Button width="full" mt={4} type="submit">
+                    Subscribe
+                  </Button>
+                </form>
+            </Center>
+        </Box>
+      </Box>
+    </Center>
+  );
+}
+
+export default App;
